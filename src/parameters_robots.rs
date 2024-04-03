648dc2bbdc4c0c6860602425b09df54fbf8ffb22
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2024-04-03 22:29:31 +0200

    Three Staubli robots added

--- src/parameters_robots.rs
+++ src/parameters_robots.rs
@@ -33,6 +33,51 @@ pub mod opw_kinematics {
             }
         }
 
+
+        // See https://www.staubli.com/content/dam/robotics/products/robots/tx2/TX2-140-160-datasheet-EN.pdf.
+        // These three Staubli robots have spherical wrist and mostly identical plan.
+        pub fn staubli_tx2_140() -> Self {
+            Parameters {
+                a1: 0.150,
+                a2: 0.000,
+                b: 0.000,
+                c1: 0.550,
+                c2: 0.625,
+                c3: 0.625,
+                c4: 0.110,
+                offsets: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                ..Self::new()
+            }
+        }
+
+        pub fn staubli_tx2_160() -> Self {
+            Parameters {
+                a1: 0.150,
+                a2: 0.000,
+                b: 0.000,
+                c1: 0.550,
+                c2: 0.825,
+                c3: 0.625,
+                c4: 0.110,
+                offsets: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                ..Self::new()
+            }
+        }
+
+        pub fn staubli_tx2_160l() -> Self {
+            Parameters {
+                a1: 0.150,
+                a2: 0.000,
+                b: 0.000,
+                c1: 0.550,
+                c2: 0.825,
+                c3: 0.925,
+                c4: 0.110,
+                offsets: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                ..Self::new()
+            }
+        }
+
         pub fn fanuc_r2000ib_200r() -> Self {
             Parameters {
                 a1: 0.720,
