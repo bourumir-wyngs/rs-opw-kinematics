c93b4023939306276f4542d44e38e4b0c6184f50
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-04-01 19:23:52 +0200

    Mesh loaders panic on malformed PLY/STL input (DoS risk)

--- src/utils/read_trimesh.rs
+++ src/utils/read_trimesh.rs
@@ -3,15 +3,16 @@
 
 use parry3d::shape::{TriMesh};
 use rs_read_trimesh::load_trimesh;
+use std::error::Error;
 
 /// Function to load a TriMesh from a PLY file
 #[deprecated(since = "1.8.4", note = "Use rs_read_trimesh::load_trimesh instead")]
-pub fn load_trimesh_from_ply(ply_file_path: &str) -> TriMesh {
-    load_trimesh(ply_file_path, 1.0).expect("Could not load mesh")
+pub fn load_trimesh_from_ply(ply_file_path: &str) -> Result<TriMesh, Box<dyn Error>> {
+    load_trimesh(ply_file_path, 1.0).map_err(|e| Box::new(e) as Box<dyn Error>)
 }
 
 /// Function to load a TriMesh from an STL file
 #[deprecated(since = "1.8.4", note = "Use rs_read_trimesh::load_trimesh instead")]
-pub fn load_trimesh_from_stl(stl_file_path: &str) -> TriMesh {
-    load_trimesh(stl_file_path, 1.0).expect("Could not load mesh")    
+pub fn load_trimesh_from_stl(stl_file_path: &str) -> Result<TriMesh, Box<dyn Error>> {
+    load_trimesh(stl_file_path, 1.0).map_err(|e| Box::new(e) as Box<dyn Error>)
 }
