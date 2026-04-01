18238017c52458831fa3b359c53005a948300a99
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-04-01 19:30:16 +0200

    Frame Kinematics applies transforms in wrong order

--- src/frame.rs
+++ src/frame.rs
@@ -147,34 +147,34 @@ impl Frame {
 
 impl Kinematics for Frame {
     fn inverse(&self, tcp: &Pose) -> Solutions {
-        self.robot.inverse(&(tcp * self.frame.inverse()))
+        self.robot.inverse(&(self.frame.inverse() * tcp))
     }
 
     fn inverse_5dof(&self, tcp: &Pose, j6: f64) -> Solutions {
-        self.robot.inverse_5dof(&(tcp * self.frame.inverse()), j6)
+        self.robot.inverse_5dof(&(self.frame.inverse() * tcp), j6)
     }
 
     fn inverse_continuing_5dof(&self, tcp: &Pose, previous: &Joints) -> Solutions {
-        self.robot.inverse_continuing_5dof(&(tcp * self.frame.inverse()), previous)
+        self.robot.inverse_continuing_5dof(&(self.frame.inverse() * tcp), previous)
     }
 
     fn inverse_continuing(&self, tcp: &Pose, previous: &Joints) -> Solutions {
-        self.robot.inverse_continuing(&(tcp * self.frame.inverse()), previous)
+        self.robot.inverse_continuing(&(self.frame.inverse() * tcp), previous)
     }
 
     fn forward(&self, qs: &Joints) -> Pose {
         // Calculate the pose of the tip joint using the robot's kinematics
         let tip_joint = self.robot.forward(qs);
         
-        tip_joint * self.frame
+        self.frame * tip_joint
     }
 
     fn forward_with_joint_poses(&self, joints: &Joints) -> [Pose; 6] {
         // Compute the forward kinematics for the robot itself
         let mut poses = self.robot.forward_with_joint_poses(joints);
 
-        // Apply the tool transformation only to the last pose (TCP pose)
-        poses[5] *= self.frame;
+        // Apply the frame transformation only to the last pose (TCP pose)
+        poses[5] = self.frame * poses[5];
 
         poses
     }    
